525fcabc9818c24fe519e99f21c69864db3d0a3e
Hanjun Kim <hallazzang@gmail.com>
2024-08-25 06:46:56 +0900

    refactor: use python app and session

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = b"exampledatasource"
+
+users = {
+    "hallazzang": {
+        "name": "Hanjun Kim",
+        "age": 33,
+        "gender": "male",
+        "salary": 3000,
+        "role": "dev",
+        "work_years": 8,
+    },
+    "yeonga": {
+        "name": "Yeonga Jang",
+        "age": 26,
+        "gender": "female",
+        "salary": 5000,
+        "role": "pm",
+        "work_years": 2,
+    },
+    "ryoochan": {
+        "name": "Chan Ryoo",
+        "age": 27,
+        "gender": "male",
+        "salary": 9000,
+        "role": "design",
+        "work_years": 3,
+    },
+}
+
+@app.get("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    username = request.form["username"]
+    session["username"] = username
+    return redirect(url_for("home"))
+
+@app.get("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("home"))
+
+@app.get("/me")
+def me():
+    username = session.get("username")
+    if not username:
+        return "You are not logged in", 401
+    return users.get(username)
